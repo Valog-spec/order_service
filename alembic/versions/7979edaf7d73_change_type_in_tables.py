8cd3f35a4d02b1405b34c2066653c57e123d8076
Victor Sagalakov <v.sagalakov@botsarmy.tech>
2026-04-01 14:36:23 +0700

    fix(): change alembic version

--- alembic/versions/7979edaf7d73_change_type_in_tables.py
+++ alembic/versions/7979edaf7d73_change_type_in_tables.py
@@ -35,7 +35,7 @@ def upgrade() -> None:
         existing_type=sa.VARCHAR(length=20),
         type_=sa.Numeric(precision=10, scale=2),
         existing_nullable=False,
-        postgresql_using="amount::numeric(10,2)"
+        postgresql_using="amount::numeric(10,2)",
     )
     # ### end Alembic commands ###
 
@@ -49,7 +49,7 @@ def downgrade() -> None:
         existing_type=sa.Numeric(precision=10, scale=2),
         type_=sa.VARCHAR(length=20),
         existing_nullable=False,
-        postgresql_using="amount::varchar(20)"
+        postgresql_using="amount::varchar(20)",
     )
     op.alter_column(
         "orders",
@@ -57,6 +57,6 @@ def downgrade() -> None:
         existing_type=sa.String(length=255),
         type_=sa.UUID(),
         existing_nullable=False,
-        postgresql_using="idempotency_key::uuid"
+        postgresql_using="idempotency_key::uuid",
     )
     # ### end Alembic commands ###
